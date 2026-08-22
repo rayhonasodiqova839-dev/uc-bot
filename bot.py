4fc8ebcc5435753742ca56d1db911eef8153541a
rayhonasodiqova839-dev <rayhonasodiqova839@gmail.com>
2026-08-22 16:58:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from telegram.ext import (
 # =========================================================
 
 # Token endi kodda emas — Fly.io secrets orqali beriladi
-BOT_TOKEN = "8287856383:AAF2mKHyEHqlL01LRyRGZ6IgmIGYRjyr4L0"
+BOT_TOKEN = "8900560478:AAF1JTrbvci_x4PxglSoYHAA_ICtPTpGMSE"
 
 if not BOT_TOKEN:
     raise RuntimeError(
